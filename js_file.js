aa20d27f641b94fb9be7c89629aed9ff348930d1
Иван Сутормин <ionecrew89@mail.ru>
2018-03-28 17:22:48 +0300

    Первое задание_1

---
+++ js_file.js
@@ -0,0 +1,33 @@
+var budgetQuestion = prompt("Ваш бюджет состовляет?")
+	orgName = prompt("Введите название своего магазина")
+
+var mainList = {
+	_budget: '30000',
+	_orgName: 'ТоргСклад',
+	shopGoods: ["продукты", "капцелярия"],
+	employers: {
+		name: 'Василий',
+		position: 'Директор',
+		year: '30'
+	}
+}
+
+var shopGoods=[] 
+shopGoodsDialog = prompt("Какой тип товара будем продавать?");
+shopGoods.push(shopGoodsDialog);
+shopGoodsDialog = prompt("Какой тип товара будем продавать?");
+shopGoods.push(shopGoodsDialog);
+shopGoodsDialog = prompt("Какой тип товара будем продавать?");
+shopGoods.push(shopGoodsDialog);
+
+console.log(shopGoods);
+
+console.log(typeof(budgetQuestion));	
+
+var budget = Number(budgetQuestion);
+
+console.log(typeof(budget));
+
+budgetOneDay = budget / 30;
+
+alert('Ваш бюджет на день состовляет: ' + budgetOneDay);
