992218532cb043b2ccf062ee31cfce4c6db9d5b2
Иван Сутормин <ionecrew89@mail.ru>
2018-04-08 16:26:14 +0300

    lesson_5 2_practic правки

--- lesson_5/DOM/2_practic/js/script.js
+++ lesson_5/DOM/2_practic/js/script.js
@@ -1,29 +1,53 @@
 let btnOpen = document.getElementById('open-btn'),
-	allLeftField = document.querySelectorAll('div[class$="-value"]'),
+	nameValue = document.querySelectorAll('.name-value'),
+	budgetValue = document.querySelectorAll('.budget-value'),
+	goodsValue = document.querySelectorAll('.goods-value'),
+	itemsValue = document.querySelectorAll('.items-value'),
+	employersValue = document.querySelectorAll('.employers-value'),
+	discountValue = document.querySelectorAll('.discount-value'),
+	isopenValue = document.querySelectorAll('.isopen-value'),
 	productСategory = document.querySelectorAll('.goods-item'),
-	threeButton = document.getElementsByTagName('button'),
-	itemsValue = document.querySelector('.items-value'),
-	timeValue = document.querySelector('.time-value'),
-	countBudget = document.querySelector('.count-budget-value'),
+	goodsItemBtn = document.getElementsByTagName('button')[0],
+	countBudgetBtn = document.getElementsByTagName('button')[1],
+	hireEmployersBtn = document.getElementsByTagName('button')[2],
+	goodsItem = document.querySelectorAll('.goods-item'),
+	timeValue = document.querySelector('#time'),
+	countBudget = document.querySelector('#budget'),
 	employers = document.querySelectorAll('.hire-employers-item');
 
 
 console.log('++++++++++++++');
-console.log(btnOpen);
+console.log(btnOpen),
 console.log('++++++++++++++');
-console.log(allLeftField);
-console.log('++++++++++++++');
-console.log(productСategory);
-console.log('++++++++++++++');
-console.log(itemsValue);
-console.log('++++++++++++++');
-console.log(timeValue);
-console.log('++++++++++++++');
-console.log(countBudget);
-console.log('++++++++++++++');
-console.log( productСategory[0], productСategory[1], productСategory[2], productСategory[3]);
-console.log('++++++++++++++');
-console.log(employers);
-console.log('++++++++++++++');
+	console.log(nameValue),
+	console.log('++++++++++++++');
+	console.log(budgetValue),
+	console.log('++++++++++++++');
+	console.log(goodsValue),
+	console.log('++++++++++++++');
+	console.log(itemsValue),
+	console.log('++++++++++++++');
+	console.log(employersValue),
+	console.log('++++++++++++++');
+	console.log(discountValue),
+	console.log('++++++++++++++');
+	console.log(isopenValue),
+	console.log('++++++++++++++');
+	console.log(productСategory),
+	console.log('++++++++++++++');
+	console.log(goodsItemBtn),
+	console.log('++++++++++++++');
+	console.log(countBudgetBtn),
+	console.log('++++++++++++++');
+	console.log(hireEmployersBtn),
+	console.log('++++++++++++++');
+	console.log(itemsValue),
+	console.log('++++++++++++++');
+	console.log(goodsItem),
+	console.log('++++++++++++++');
+	console.log(countBudget),
+	console.log('++++++++++++++');
+	console.log(employers);
+	console.log('++++++++++++++');
 
 
