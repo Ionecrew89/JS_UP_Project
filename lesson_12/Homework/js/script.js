76fb684a875fb3844a1940d6217b76911eb6974e
Иван Сутормин <ionecrew89@mail.ru>
2018-04-15 21:20:47 +0300

    правки lesson_12

--- lesson_12/Homework/js/script.js
+++ lesson_12/Homework/js/script.js
@@ -1,36 +1,50 @@
 $(document).ready(function() {
 	
+    $('[class^="main_bt"]').on('click', function () {
+		$('.overlay').fadeIn(900);
+		$('.modal').slideDown(500);
+	});
+	$('nav li:eq(1)').on('click', function () {
+		$('.overlay').fadeIn(900);
+		$('.modal').slideDown(500);
+	});
+	$('.close').on('click', function () {
+		$('.overlay').fadeOut(900);
+		$('.modal').slideUp(500);
+	})
+
+
 // $('.main_btn')
 // $('.main_btna')
 // $('[href="#tour"]')
 // $('[href="#sheldure"]')
 
-	$('.main_btna').on('click', function() {
+	// $('.main_btna').on('click', function() {
 
-		let modal = $('.modal'),
-			overlay = $('.overlay')		
+	// 	let modal = $('.modal'),
+	// 		overlay = $('.overlay')		
 
-		modal.css('margin-top', '-600px');
-		modal.fadeIn();
-		overlay.fadeIn(3000);
+	// 	modal.css('margin-top', '-600px');
+	// 	modal.fadeIn();
+	// 	overlay.fadeIn(3000);
 	    
-		modal.animate(
-			{
-				'margin-top': '5rem'
+	// 	modal.animate(
+	// 		{
+	// 			'margin-top': '5rem'
 
-			}, 2500
-		);
+	// 		}, 2500
+	// 	);
 
-		$('.close').click(function() {
+	// 	$('.close').click(function() {
 
-			overlay.fadeOut(3000);
-			modal.animate(
-				{
-					'margin-top': '-600px'
+	// 		overlay.fadeOut(3000);
+	// 		modal.animate(
+	// 			{
+	// 				'margin-top': '-600px'
 
-				}, 2500
-			);
-			modal.fadeOut();
-		});
-	});
+	// 			}, 2500
+	// 		);
+	// 		modal.fadeOut();
+	// 	});
+	// });
 });
