9aa2ec53d7f7aa0de24d9647f454d2dacffd9504
Иван Сутормин <ionecrew89@mail.ru>
2018-04-15 23:38:46 +0300

    lesson_13

--- lesson_13/js/script.js
+++ lesson_13/js/script.js
@@ -255,6 +255,45 @@ let slideIndex = 1,
 
 
 	// Calc
-	
+	let persons = document.getElementsByClassName('counter-block-input')[0],
+		restDays = document.getElementsByClassName('counter-block-input')[1],
+		place = document.getElementById('select')
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
 
+	totalValue.innerHTML = 0;
+	persons.addEventListener('change', function(){
+		personsSum = +this.value;
+		total = (daysSum + personsSum)*4000;
+		
+		if(restDays.value == '' || persons.value == '' || restDays.value < 1 || !Number.isInteger(restDays.value)) {
+			totalValue.innerHTML = 0;	
+
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	restDays.addEventListener('change', function(){
+		daysSum = +this.value;
+		total = (daysSum + personsSum)*4000;
+		
+		if(restDays.value == '' || persons.value == '' || persons.value < 1 || !Number.isInteger(persons.value)) {
+			totalValue.innerHTML = 0;	
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	place.addEventListener('change', function() {
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;	
+		} else {
+			let a = total;
+			
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	});
 });
