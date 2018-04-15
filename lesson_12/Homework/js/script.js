887b63547d0f3aaf840cccd293666a9587edb39e
Иван Сутормин <ionecrew89@mail.ru>
2018-04-15 17:20:46 +0300

    lesson12

---
+++ lesson_12/Homework/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+// $('.main_btn')
+// $('.main_btna')
+// $('[href="#tour"]')
+// $('[href="#sheldure"]')
+
+	$('.main_logo').on('click', function() {
+
+		let modal = $('.modal'),
+			overlay = $('.overlay')		
+
+		modal.css('margin-top', '-600px');
+		modal.fadeIn();
+		overlay.fadeIn(3000);
+	    
+		modal.animate(
+			{
+				'margin-top': '5rem'
+
+			}, 2500
+		);
+
+		$('.close').click(function() {
+
+			overlay.fadeOut(3000);
+			modal.animate(
+				{
+					'margin-top': '-600px'
+
+				}, 2500
+			);
+			modal.fadeOut();
+		});
+	});
+});
