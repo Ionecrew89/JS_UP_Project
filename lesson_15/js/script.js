07c2ddf7f99905d55ed86d35333a1a3b16186a29
Иван Сутормин <ionecrew89@mail.ru>
2018-04-25 01:20:32 +0300

    lesson_15_3

--- lesson_15/js/script.js
+++ lesson_15/js/script.js
@@ -5,9 +5,14 @@ window.addEventListener('DOMContentLoaded',function() {
 		popup_btn = document.getElementsByClassName('popup-btn')[0],
 		main = document.getElementsByClassName('main')[0],
 		custom = document.getElementsByClassName('custom')[0],
+		// custom_char = custom.querySelector('.custom-char'),
 		body = document.getElementsByTagName('body')[0],
 		btn_ready = document.getElementById('ready'),
 		main_cards_item = document.querySelectorAll('.main-cards-item')[0];
+		// ворма создания кандидата
+		// custom_info = document.querySelector('.custom-info'),
+		// custom_info_clone = custom_info.cloneNode(true);
+
 
 		// сбрасываем выделение кандлидата 
 		main_cards_item.classList.remove('main-cards-item-active');
@@ -75,28 +80,37 @@ let custom_info_form = document.getElementsByClassName('custom-info')[0],
 	
 
 	// Выбор пола
-	function radioChecked() { 
+	function radioChecked(defaltSlider = 0) { 
+		this.defaltSlider = defaltSlider;
 
-		for(let i = 0; i < form_radio_input.length; i++) {		
+		function checkedClear() {
+			form_radio_input[0].removeAttribute('checked');
+			form_radio_input[1].removeAttribute('checked');
+		}	
+			
+		if (defaltSlider !== 1){
+		
+			for(let i = 0; i < form_radio_input.length; i++) {		
 
-			form_radio_input[i].addEventListener('click', function() {
+				form_radio_input[i].addEventListener('change', function() {
 
-				// переключение радио в между полами
-				if(this.hasAttribute('checked')) {
+					// переключение радио в между полами
+					if(this.hasAttribute('checked')) {
 
-					} else {
+						} else {
 
-					function checkedClear() {
-						form_radio_input[0].removeAttribute('checked');
-						form_radio_input[1].removeAttribute('checked');
+						checkedClear();
+						this.setAttribute('checked', '');	
 					}
-					checkedClear();
-					this.setAttribute('checked', '');	
-				}
 
-				form_radio_value = this;
-				slider();
-			});	
+					form_radio_value = this;
+					slider();
+				});	
+			}
+
+		} else {
+			checkedClear();
+			form_radio_input[0].setAttribute('checked', '');
 		}	
 	}
 
@@ -132,6 +146,7 @@ let custom_info_form = document.getElementsByClassName('custom-info')[0],
 		let person_easy = document.querySelector('.person-easy');
 					
 		// Выбор галереи 
+		console.log(form_radio_value.value + '+++++++++++++')
 		if(form_radio_value.value == "Мужской") {
 
 			var slideIMG = slideIMG_men;
@@ -193,34 +208,49 @@ let custom_info_form = document.getElementsByClassName('custom-info')[0],
 		};
 	};
 
-
+		
 	// Создаем карточку и переносим все данные
 	btn_ready.addEventListener('click', ()=>{
+			// ФИО
+		let i1 = custom_info_form.querySelectorAll('input[type="text"]')[0].value,
+			// Возраст
+			i2 = custom_info_form.querySelectorAll('input[type="text"]')[0].value,
+			// Библиография
+			i3 = custom_info_form.querySelector('textarea');
+			reg = /[a-zA-Z]/i;
+			number = /^(\-|\+)?([0-9]+|Infinity)$/;
+			// console.log(number.test(i2));
+		// Проверки для полей
+		if ( reg.test(i1) || i1 == '' || i1.length < 4 || i2 < 35 || i2 > 80 || number.test(i2) || i3.length < 10) {
 
-		// отображаем необходимые  элементы
-		main.style.display = 'block';
-		custom.classList.remove('active');
-		hiddedElementsStart('block','none');
+			alert('Проверьте данные на корректность\n\r- Все поля обязательны для заполнения\n\r - ФИО - только русские буквы и не меньше 4 символов.\n\r - Возраст от 35 до 80\n\r Библиография не меньше 10 символов\n\r');
 
-		// создаём карточку
-		main_cards.insertBefore(main_cards_it_clon, main_cards_it[0]);
+		} else {
+			
+			// отображаем необходимые  элементы
+			main.style.display = 'block';
+			custom.classList.remove('active');
+			hiddedElementsStart('block','none');
 
-		// перенос данных из формы в карточку
-		candidate_name.textContent = form_name_value.value;
-		candidate_age.textContent = form_age_value.value + ' лет';
-		candidate_sex.textContent = form_radio_value.value;
-		candidate_views.textContent = form_select_value.value;
-		candidate_bio.textContent = form_bio_value.value;
+			// создаём карточку
+			main_cards.insertBefore(main_cards_it_clon, main_cards_it[0]);
 
-		// перенос фото
-		let photo_new = document.getElementsByClassName('photo-1')[1];
-		photo_new.style.backgroundImage = _slideIMG;
-		photo_new.style.backgroundSize = 'contain';
+			// перенос данных из формы в карточку
+			candidate_name.textContent = form_name_value.value;
+			candidate_age.textContent = form_age_value.value + ' лет';
+			candidate_sex.textContent = form_radio_value.value;
+			candidate_views.textContent = form_select_value.value;
+			candidate_bio.textContent = form_bio_value.value;
 
-		// Очищаем результаты
-		resultClean()
+			// перенос фото
+			let photo_new = document.getElementsByClassName('photo-1')[1];
+			photo_new.style.backgroundImage = _slideIMG;
+			photo_new.style.backgroundSize = 'contain';
+
+			// Очищаем результаты
+			resultClean()
+		}
 	});
-
 
 
 		// Кнопка Сбросить результаты
@@ -240,50 +270,58 @@ let custom_info_form = document.getElementsByClassName('custom-info')[0],
 		custom.classList.add('active');
 
 		main_cards_it_clon.remove();
+
+		// Очищаем поля формы создания
+		form_name_value.value = '';
+			 form_age_value.value = '';
+			 form_select_value.value = 'Либеральные';
+			 form_bio_value.value = '';
+			 // form_radio_value.value = "Мужской"
+			 radioChecked(1);
+			 slider();
+
 	});
 
-	// btnVoting.addEventListener('click', ()=>{
+	// генерация случайных числе - выборы же "честные"))))
+	function randomNumber(max,min) {
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	};
 
-	// 	resultClean(70,70);
-	// });
+	function random(crime = 0) {
+		this.crime = crime,
+		a = randomNumber(1, 100),
+		b = randomNumber(1, 100),
+		c = randomNumber(1, 100),
+		sum = a + b + c;
+		while(sum !== 100) {
+			a = randomNumber(1, 100),
+			b = randomNumber(1, 100) + crime,
+			c = randomNumber(1, 100),
+			sum = a + b + c;
+		} 
+	} ;
 
 	// Кнопка Честное голосование
 	btnVoting.addEventListener('click', function () {
-		// генерация случайных числе - выборы же "честные"))))
-		let a = Math.floor(Math.random() * 100 + 0),
-			b = Math.floor(Math.random() * (100 - a) + 0),
-			c = 100 - (a + b);
-		
-		result_count[0].textContent = a + '%';
-		result_count[1].textContent = b + '%';
-		result_count[2].textContent = c + '%';
 
-		progress_line[0].style.height = a + '%';
-		progress_line[1].style.height = b + '%';
-		progress_line[2].style.height = c + '%';
+			randomNumber();
+			random();
+		
+			result_count[0].textContent = a + '%';
+			result_count[1].textContent = b + '%';
+			result_count[2].textContent = c + '%';
+
+			progress_line[0].style.height = a + '%';
+			progress_line[1].style.height = b + '%';
+			progress_line[2].style.height = c + '%';
+		
 	});		
 
 	// Вмешаться в выборы
 	btnCrime.addEventListener('click', function () {
 
-		let a = Math.floor(Math.random() * 100 + 0),
-			b = Math.floor(Math.random() * (100 - a) + 0),
-			c = 100 - (a + b);
-
-			 if(a >= 25 && c >= 25) {
-			 	a = a - 15;
-				b = b + 25;
-				c = c - 15;
-
-			 	} else if(a >= 25) {
-					a = a - 25;
-					b = b + 25;
-					
-					} else if(c >= 25) {
-						c = c - 25;
-						b = b + 25;
-					
-			};
+		randomNumber();
+		random(25);
 
 		result_count[0].textContent = a + '%';
 		result_count[1].textContent = b + '%';
